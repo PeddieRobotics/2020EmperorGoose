ad30ecbd487ad097a091669b9cc0db448f8d4a71
Elizabeth Hu <ehu-21@peddie.org>
2020-02-01 14:22:57 -0500

    created sensor method

--- src/main/java/frc/robot/Commands/NeckSpin.java
+++ src/main/java/frc/robot/Commands/NeckSpin.java
@@ -23,23 +23,34 @@ public class NeckSpin extends CommandBase {
   @Override
   public void initialize() {
   }
+// tells whether sensor sees bol
+private boolean senses_ball(AnalogInput sensor)
+{
+  if (sensor.getVoltage() > 3) 
+  {
+    return true;
+  }
+  else 
+  {
+    return false;
+  }
+}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (analogT.getVoltage() < 3 && analogB.getVoltage() <3) {
-      neck.runMotors(15.0);
-    }
-
-    if (analogT.getVoltage() < 3 && analogB.getVoltage() > 3) {
-      neck.runMotors(15.0);
-    }
-
-    if (analogT.getVoltage() > 3 && analogB.getVoltage() < 3) {
-      neck.runBottomMotor(15.0);
-    }
+// Called every time the scheduler runs while the command is scheduled.
+@Override
+public void execute() {
+  if (!senses_ball(analogT) && !senses_ball(analogB)) {
+    m_neck.runMotors(15.0);
   }
 
+  if (analogT.getVoltage() < 3 && analogB.getVoltage() > 3) {
+    m_neck.runMotors(15.0);
+  }
+
+  if (analogT.getVoltage() > 3 && analogB.getVoltage() < 3) {
+    m_neck.runBottomMotor(15.0);
+  }
+}
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
