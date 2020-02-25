be1a66a66a28297ba355e5c9d2b571cfaaa55ce5
gwkoch <58572473+gwkoch@users.noreply.github.com>
2020-02-25 17:10:23 -0500

    changed tower indexing speeds
    still no limelight
    reduced the motor speeds on the tower-- 0.4 was causing jams

--- src/main/java/frc/robot/commands/TowerCommands/IndexPowerCells.java
+++ src/main/java/frc/robot/commands/TowerCommands/IndexPowerCells.java
@@ -38,7 +38,7 @@ public class IndexPowerCells extends CommandBase {
         m_tower.printSensorVolts();
         if(!m_tower.senses_ball_Top1()){
           DriverStation.reportError("running all",false);
-          m_tower.runMotors(.8);
+          m_tower.runMotors(.25); // reduced to prevent over-indexing when the flywheel is spinning down
           m_hopper.runAll();
         }
         else if(m_tower.senses_ball_Top0()){
@@ -47,7 +47,7 @@ public class IndexPowerCells extends CommandBase {
           m_hopper.stopAll();
         }
         else if(!m_tower.senses_ball_Bottom()){
-          m_tower.runBottomMotor(0.8);
+          m_tower.runBottomMotor(0.35); // reduced to prevent over-indexing when the flywheel is spinning down
           DriverStation.reportError("running bottom", false);
           m_hopper.runAll();
         }
