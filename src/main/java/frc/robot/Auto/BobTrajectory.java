cac32ee06235763fedf7b19267ab4e076c585468
VishnuGade <58572549+VishnuGade@users.noreply.github.com>
2020-03-01 10:18:58 -0700

    fixed code in desc
    - auto better
    - shooting bounds more strict

--- src/main/java/frc/robot/Auto/BobTrajectory.java
+++ src/main/java/frc/robot/Auto/BobTrajectory.java
@@ -4,9 +4,9 @@ import com.team319.trajectory.RobotConfig;
 import com.team319.ui.BobTrajectoryApp;
 
 public class BobTrajectory {
-      
+
     public static void main(String... args) {
-        RobotConfig.dt = 0.02;//we wanna gen. paths at 200hz(dt is in seconds, so this is a 5ms execute)
+        RobotConfig.dt = 0.01;//we wanna gen. paths at 200hz(dt is in seconds, so this is a 5ms execute)
         new BobTrajectoryApp();
     }
 }
